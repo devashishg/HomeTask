e75c32f08d3d85f4470e22713686febf9990ff23
amanrajs <amanraj464@gmail.com>
2019-10-12 23:54:43 +0530

    JSoN File On Server

---
+++ index.js
@@ -0,0 +1,22 @@
+let head = document.getElementById("head");
+head.style.background="#11254d";
+head.style.padding="15px";
+head.style.color ="white";
+head.style.fontSize="25px";
+
+let smallText  = document.getElementsByTagName("small");
+
+
+
+let viewArea = document.getElementById("viewArea");
+// viewArea.write()
+
+
+// var request = new XMLHttpRequest();
+// request.open('GET','url');
+// request.onload = ()=>{
+//     var data = JSON.parse(request.responseText);
+//     console.log(data);
+// };
+// request.send();
+
