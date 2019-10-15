dba8a03dc5aeb4f8f45e41bad44f5bdddc42105f
Devashish <Devashish_gupta@epam.com>
2019-10-15 15:51:14 +0530

    complete except saving

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var set = new Set();
+localStorage.removeItem('flag');
+if(localStorage.getItem('flag')!=0){
+    console.log('hi ');
+    var on = '{"array":[]}';
+    var ob = JSON.parse(on);
+    localStorage.setItem('flag',0);
+    localStorage.setItem('Subscriptions',JSON.stringify(ob));
+}
+
 
 //Header Only
 let headr = document.getElementById("head");
@@ -25,11 +34,14 @@ box.appendChild(conTent);
 var data;
 let main = document.getElementById("main");
 main.className= "article-container";
+main.style.border='none';
 
 //List of Stories
 let story = document.createElement("div");
 story.className= "seventy";
 document.body.insertBefore(box, document.body.firstChild);
+
+let form = document.createElement("form");
 
 
 //HTTP Request
@@ -37,6 +49,26 @@ var request = new XMLHttpRequest();
 request.open('GET','https://devashishg.github.io/HomeTask/file.json');
 request.onload = ()=>{
     data = JSON.parse(request.responseText);
+    loadElement(data);
+
+
+    //Button event listener using classname
+    /*var btns = document.querySelectorAll('.button1');
+    for (j = 0; j < btns.length; j++) { 
+    var id = btns[j].getAttribute('id');
+    btns[j].addEventListener('click', ()=> {
+        console.log('Triggered');
+        box.style.display = "block";   
+        conTent.innerHTML = `<h1>${data.array[Number(id)].title}</h1><br/>${data.array[Number(id)].Description}`;
+    });}*/
+
+
+};
+request.send();
+
+
+var loadElement = (data)=>{
+
     //Loop to iterate JSOn
     for(i=0;i<data.array.length;i++){
 
@@ -56,7 +88,7 @@ request.onload = ()=>{
         //Json Data Filling var set4 = new Set();
         titleText.innerHTML = `${data.array[i].title}`;
         Descr.innerHTML = `${data.array[i].Description.substring(0,300)}....`;
-        timeCatText.innerHTML = `Posted on ${data.array[i].Date} //  Category:<b>${data.array[i].Category}</b>`;
+        timeCatText.innerHTML = `Posted on ${data.array[i].Date} //  Category:${data.array[i].Category}`;
 
         //image 
 
@@ -71,6 +103,7 @@ request.onload = ()=>{
         //One complete Story
         let oneArticle = document.createElement('div');
         oneArticle.className = 'article-container hundred';
+
         let contentStory = document.createElement("div");  // content only 
         contentStory.className="seventy";
         contentStory.appendChild(titleText);
@@ -81,11 +114,14 @@ request.onload = ()=>{
         
         oneArticle.appendChild(storywall);
         oneArticle.appendChild(contentStory);
+        
 
         //story list 
-        story.appendChild(oneArticle);
-        if(i<data.array.length-1){
-        story.appendChild( document.createElement('hr'));}
+        
+        if(i==data.array.length-1){
+            oneArticle.style.border='none';
+        }
+        story.appendChild(oneArticle);    
     }
 
     main.appendChild(story);
@@ -94,41 +130,53 @@ request.onload = ()=>{
 
     let form = document.createElement("form");
     form.setAttribute('action','#');
-    form.className = 'rest';  
+    form.className = 'rest';
+    form.style.paddingLeft= '3%';  
+    form.setAttribute('onSubmit',`save(this)`);
+    
+    let selectTitle =document.createElement('span');
+    selectTitle.innerHTML = '<h3>SELECT CATEGORY</h3>';
+    form.appendChild(selectTitle);
+
+    //select Field
 
     let select =document.createElement('Select');
     let option=document.createElement('Option');
-        option.setAttribute('value','All');
-        option.innerHTML = 'All';
-        select.appendChild(option);
+    option.setAttribute('value','All');
+    option.innerHTML = 'All';
+    select.appendChild(option);
     set.forEach((i)=>{
         let option=document.createElement('Option');
         option.setAttribute('value',i);
         option.innerHTML = i;
         select.appendChild(option);
     });
-    
     select.setAttribute('name','category');
+    select.setAttribute('onchange',`filter(this)`);
     form.appendChild(select);
+
+    let SubscribeLabel =document.createElement('span');
+    SubscribeLabel.innerHTML = '<h3>SUBSCRIBE</h3>';
+    form.appendChild(SubscribeLabel);
     
+    //Subscribe Button and field
+    let inputdiv =document.createElement('div');
+    inputdiv.className="form-inline";
+    let input =document.createElement('input');
+    input.setAttribute("type","email");
+    input.setAttribute("name","email");
+    input.setAttribute("placeholder","Email Address");
+    input.required=true;
+    inputdiv.appendChild(input);
+    let Subscribe =document.createElement('button');
+    Subscribe.setAttribute("type","submit");
+    Subscribe.innerHTML="SUBSCRIBE";
+    inputdiv.appendChild(Subscribe);
     
+    form.appendChild(inputdiv);
     main.appendChild(form);
     //console.log(data); 
-
-
-    //Button event listener using classname
-    /*var btns = document.querySelectorAll('.button1');
-    for (j = 0; j < btns.length; j++) { 
-    var id = btns[j].getAttribute('id');
-    btns[j].addEventListener('click', ()=> {
-        console.log('Triggered');
-        box.style.display = "block";   
-        conTent.innerHTML = `<h1>${data.array[Number(id)].title}</h1><br/>${data.array[Number(id)].Description}`;
-    });}*/
-
-
-};
-request.send();
+}
 
 //footer
 let foot = document.getElementById("foot");
@@ -141,11 +189,17 @@ footer.innerHTML = "<center>© Newsfeed 2019</center>";
 foot.appendChild(footer);
 
 
+// footer.style.padding="20px";
+// footer.style.color ="white";
+// footer.style.position ="fixed";
+// footer.style.bottom ='0';
+// foot.style.background="#11254d";
+// foot.style.width="100%";
 
-
+//Button onclick method using id
 var load = (i)=>{
         let obj=data.array[i];
-    //console.log(obj[1]);
+        //console.log(obj[1]);
         box.style.display = "block";   
         conTent.innerHTML = `<h1>${obj.title}</h1><br/>${obj.Description}`;
 }
@@ -156,3 +210,34 @@ window.onclick = function(event) {
       box.style.display = "none";
     }
   }
+
+
+function filter(obj){
+    var str=obj.value;
+    var elements = document.querySelectorAll('.hundred');
+    //console.log(elements);
+    for (j = 0; j < elements.length; j++) {
+        // console.log(elements[j].childNodes[1].childNodes[1].innerHTML.split(":")[1]);
+        // console.log(str);
+        if(elements[j].childNodes[1].childNodes[1].innerHTML.split(':')[1] != str && str!='All'){
+            elements[j].style.display = 'none';
+        }else{
+            elements[j].style.display = 'flex';
+        }
+        if(str=='All'){
+            elements[j].style.display = 'flex';
+        }
+    }
+}
+
+
+function save (obj1){
+    console.log(localStorage.getItem('Subscriptions'));
+    var subsArray = JSON.parse(localStorage.getItem('Subscriptions'));
+    subsArray.array[subsArray.array.length]=obj1.email.value;
+    console.log(subsArray.array[0]);
+    localStorage.setItem('Subscriptions',subsArray);
+    alert(`Hi ${obj1.email.value.split("@")[0]}, Thankyou for Subscribing!`);
+}
+
+
